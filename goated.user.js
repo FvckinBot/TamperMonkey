87fda9922b2db59871fdc3e43ff913ac42b2e59e
FvckinBot <asudahtai88@gmail.com>
2025-07-17 03:24:35 +0700

    Update goated.user.js

--- goated.user.js
+++ goated.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
-// @name         Stylish Popup Example
-// @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Demonstrates injecting a professional popup into any page
-// @author       YourName
-// @match        *://*/*
-// @grant        GM_addStyle
-// ==UserLibrary==
-// @license MIT
+// @name          My Script
+// @namespace     http://www.example.com/gmscripts
+// @description   Scripting is fun
+// @include       http://www.example.com/*
+// @include       http://www.example.org/*
+// @exclude       http://www.example.org/foo
+// @require       foo.js
+// @resource      resourceName1 resource1.png
+// @resource      resourceName2 http://www.example.com/resource2.png
+// @version       1.0
+// @icon          http://www.example.net/icon.png
 // ==/UserScript==
-// ==/UserLibrary==
 (function() {
     'use strict';
 
